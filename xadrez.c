7288118a353a02f7ee9c6c089c4d6902bdd89588
Fer358-estacio <202504578404@alunos.estacio.br>
2025-09-09 23:29:17 +0000

    constante de movimentaçao ok

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    // ==========================
+    // 
     // Constantes de movimentação
-    // ==========================
+    // 
     const int BISPO_MOV = 5;     // casas para o bispo
     const int TORRE_MOV = 5;     // casas para a torre
     const int RAINHA_MOV = 8;    // casas para a rainha
 
-    
+
