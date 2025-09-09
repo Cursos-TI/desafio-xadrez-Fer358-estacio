00e7dbf133c0ff4687a140391de7055b94bb2c53
Fer358-estacio <202504578404@alunos.estacio.br>
2025-09-09 23:31:20 +0000

    bispo ok

--- xadrez.c
+++ xadrez.c
@@ -8,4 +8,15 @@ int main() {
     const int TORRE_MOV = 5;     // casas para a torre
     const int RAINHA_MOV = 8;    // casas para a rainha
 
+     // 
+    // Bispo - usando FOR
+    // 
+    printf("=== Movimentação do Bispo (FOR) ===\n");
+    for (int i = 1; i <= BISPO_MOV; i++) {
+        // Bispo se move na diagonal superior direita (cima + direita)
+        printf("Passo %d: CIMA + DIREITA\n", i);
+    }
+
+    
+
 
